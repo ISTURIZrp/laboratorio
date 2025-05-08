672e886825eed11edcef90e9b548aa09a65bd021
ISTURIZrp <136546648+ISTURIZrp@users.noreply.github.com>
2025-05-08 11:46:36 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './utilities/AuthContext';
 import AuthLoginForm from './components/AuthLoginForm';
 import LayoutMainDashboard from './components/LayoutMainDashboard';
+import InventorySection from './components/InventorySection';
+import EquipmentSection from './components/EquipmentSection';
+import EquipmentMonitoringSection from './components/EquipmentMonitoringSection';
+import TransactionsSection from './components/TransactionsSection';
+import ReportsSection from './components/ReportsSection';
+import EntriesSection from './components/EntriesSection';
+import './styles.css';
 
 const App = () => {
-  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-
   return (
-    <>
-      {!currentUser ? (
-        <AuthLoginForm />
-      ) : (
-        <LayoutMainDashboard />
-      )}
-    </>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<AuthLoginForm />} />
+          <Route path="/" element={<LayoutMainDashboard />}>
+            <Route index element={<InventorySection />} />
+            <Route path="equipment" element={<EquipmentSection />} />
+            <Route path="monitoring" element={<EquipmentMonitoringSection />} />
+            <Route path="transactions" element={<TransactionsSection />} />
+            <Route path="reports" element={<ReportsSection />} />
+            <Route path="entries" element={<EntriesSection />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
 export default App;
-
-// DONE
